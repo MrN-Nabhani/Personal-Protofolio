cdcf6f0281dd4d77887f582fb08e288a56bca380
MrN-Nabhani <nabhaninajm@gmail.com>
2020-09-20 01:34:24 +0300

    Updates

---
+++ precache-manifest.d5c537c959bd60358bb89d36c3e413e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3453fb4f26ae7108e09729acfb9c5a",
+    "url": "/Personal-Protofolio/index.html"
+  },
+  {
+    "revision": "7ac8bbb879931146cb04",
+    "url": "/Personal-Protofolio/static/css/main.3eb660f5.chunk.css"
+  },
+  {
+    "revision": "b984ef9538ad15928f5b",
+    "url": "/Personal-Protofolio/static/js/2.9c9bc095.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Personal-Protofolio/static/js/2.9c9bc095.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ac8bbb879931146cb04",
+    "url": "/Personal-Protofolio/static/js/main.a2e9d9b0.chunk.js"
+  },
+  {
+    "revision": "1877d47e25d387dafd39",
+    "url": "/Personal-Protofolio/static/js/runtime-main.cace15eb.js"
+  }
+]);
